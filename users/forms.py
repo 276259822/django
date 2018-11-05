192b2fe463f9bfd1505ff9f3bb520e35fa02614c
李华东 <276259822@qq.com>
2018-11-05 22:59:34 +0800

    users

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
+
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'password', 'email',)
+        fields = ('username', 'nickname', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['nickname'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
+
 def register(request):
-    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = RegisterForm()
+
     context = {
         'form': form
     }
