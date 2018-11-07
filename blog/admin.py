97ffd5d8df70e13aea9cec00d7d178e9b8563a93
李华东 <276259822@qq.com>
2018-11-07 13:15:52 +0800

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Tag, Article
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_time')
+    ordering = ['-created_time']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_time')
+    ordering = ['-created_time']
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    ordering = ['-created_time']
